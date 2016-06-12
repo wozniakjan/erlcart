a0a523c48b9a7195d146e50400e841c40119f51b
Jan Wozniak <wozniak.jan@gmail.com>
2016-06-12 16:36:28 -0400

    added dependencies

---
+++ template/web/deps/plug/mix.exs
@@ -0,0 +1,38 @@
+defmodule Plug.Mixfile do
+  use Mix.Project
+
+  @version "1.1.5"
+
+  def project do
+    [app: :plug,
+     version: @version,
+     elixir: "~> 1.0",
+     deps: deps,
+     package: package,
+     description: "A specification and conveniences for composable " <>
+                  "modules between web applications",
+     name: "Plug",
+     docs: [extras: ["README.md"], main: "readme",
+            source_ref: "v#{@version}",
+            source_url: "https://github.com/elixir-lang/plug"]]
+  end
+
+  # Configuration for the OTP application
+  def application do
+    [applications: [:crypto, :logger],
+     mod: {Plug, []}]
+  end
+
+  def deps do
+    [{:cowboy, "~> 1.0", optional: true},
+     {:earmark, "~> 0.1", only: :docs},
+     {:ex_doc, "~> 0.11", only: :docs},
+     {:inch_ex, ">= 0.0.0", only: :docs},
+     {:hackney, "~> 1.2.0", only: :test}]
+  end
+
+  defp package do
+    %{licenses: ["Apache 2"],
+      links: %{"GitHub" => "https://github.com/elixir-lang/plug"}}
+  end
+end
